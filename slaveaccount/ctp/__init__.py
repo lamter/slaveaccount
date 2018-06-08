6991e9b232f29443038399ad6fe617e121021c54
lamter <lamter.fu@gmail.com>
2018-06-09 00:28:29 +0800

    feat: 增加了依赖包 tradingtime

--- slaveaccount/ctp/__init__.py
+++ slaveaccount/ctp/__init__.py
@@ -1,6 +1,6 @@
 # encoding: UTF-8
 
 from __future__ import absolute_import
-from slaveaccount.ctp.vnctpmd import MdApi
-from slaveaccount.ctp.vnctptd import TdApi
+from .vnctpmd import MdApi
+from .vnctptd import TdApi
 from .ctp_data_type import defineDict
