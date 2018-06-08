48d350d1b49523ba688da69720d2d48dacddab3a
lamter <lamter.fu@gmail.com>
2018-06-08 23:29:47 +0800

    feat: 更改加载的路径

--- slaveaccount/ctp/__init__.py
+++ slaveaccount/ctp/__init__.py
@@ -1,6 +1,6 @@
 # encoding: UTF-8
 
 from __future__ import absolute_import
-from .vnctpmd import MdApi
-from .vnctptd import TdApi
+from slaveaccount.ctp.vnctpmd import MdApi
+from slaveaccount.ctp.vnctptd import TdApi
 from .ctp_data_type import defineDict
